fabf3055ceedaf3b224e92223071492cdf67a424
Sander <s.vdvondervoort@lameco.nl>
2020-04-03 20:31:42 +0200

    :sparkles: Added script

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+/**
+* Change myApplication to the projectname
+*/
+
+var wellMadeGameBlog = (function($, window, undefined) {
+
+    /**
+    * Declare all your methods here
+    */
+
+    var init;
+
+    init = function() {
+
+        /**
+         * Variables
+         */
+
+         body = $('body');
+
+        /**
+         * Change body class for header on scroll
+         */
+
+         scrollTrigger = 1;
+
+        $(window).scroll(function(){
+            if ($(this).scrollTop() >= scrollTrigger) {
+                body.addClass('body--scrolled');
+            } else {
+                body.removeClass('body--scrolled');
+            }
+        });
+
+    };
+
+    /**
+    * Put the methods you want to be public in this object
+    */
+
+    return {
+        init: init
+    };
+
+}(jQuery, window));
+
+$(function() {
+    wellMadeGameBlog.init();
+});
